66e71919d7ac3f36af56af19aea8c81995478e83
jsj4653 <jsj465377@gmail.com>
2025-07-09 17:56:41 +0900

    20250709 17:56 6번 조성재 수정

--- team01/src/useManager/ConsoleUI.java
+++ team01/src/useManager/ConsoleUI.java
@@ -1,5 +1,48 @@
 package useManager;
 
-public class ConsoleUI {
+import java.util.Scanner;
 
+public class ConsoleUI {
+	private int button = 0;
+	// 로그인 기능
+	
+	 public void Consol(Scanner sc) {
+		
+		while (true) {
+			ConsoleUI.menu("목록 확인", "대출 확인", "반납 하기");
+			button = sc.nextInt();
+			switch (button) {
+			case 1:
+				//목록확인 메소드
+				System.out.println("목록확인");
+				break;
+			case 2:
+				//대출확인 메소드
+				System.out.println("대출 확인");
+				break;
+			case 3:
+				//반납하는 메소드
+				System.out.println("반납");
+				break;	
+			default:
+				break;
+			} // switch
+			break; 
+		} // while
+	}
+
+	static public void menu(String... menuNames) {
+		int count = 1;
+		System.out.println("=================================");
+		for (String menu : menuNames) {
+			System.out.println("=\t\t\t\t=");
+			System.out.println("=\t" + count + ". " + menu);
+			System.out.println("=\t\t\t\t");
+			count++;
+		}
+
+		System.out.println("=\t0. 종료");
+		System.out.println("=\t\t\t\t");
+		System.out.println("=================================");
+	}
 }
