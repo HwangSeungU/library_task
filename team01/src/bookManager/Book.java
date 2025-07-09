9e0e6dd6955ebdd42274cf7ed7f00b5e6ff793df
seojin12lee <aillsalovesophia@gmail.com>
2025-07-09 17:57:00 +0900

    250709 17:56 07.09 학원에서 작업한 내용저장

--- team01/src/bookManager/Book.java
+++ team01/src/bookManager/Book.java
@@ -5,6 +5,60 @@ package bookManager;
 //	도서 대출 메소드 오버라이딩
 //	equals, hashCode 오버라이딩
 
-public class Book {
+public class Book extends memberManager {
+	// 필드
+	private String bookTitle; // 책 제목
+	private String author; // 작가
+	private boolean isBorrow;// 빌린여부
 
+	// 생성자
+	public Book(String bookTitle, String author, boolean isBorrow) {
+		super();
+		this.bookTitle = bookTitle;
+		this.author = author;
+		this.isBorrow = isBorrow;
+	}
+
+	/*--------오버라이딩-----------*/
+	// hashCode
+	@Override
+	public int hashCode() {
+		//
+		return super.hashCode();
+	}
+
+	// equals
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	// 도서 대출 메소드
+
+	// getter, setter
+	public String getBookTitle() {
+		return bookTitle;
+	}
+
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public boolean isBorrow() {
+		return isBorrow;
+	}
+
+	public void setBorrow(boolean isBorrow) {
+		this.isBorrow = isBorrow;
+	}
+
+	
 }
