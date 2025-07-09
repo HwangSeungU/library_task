924ed5877c501dde88db9fd984050fb408c78bf5
seojin12lee <aillsalovesophia@gmail.com>
2025-07-09 12:40:06 +0900

    250709 12:39 lsj brach push test

--- team01/src/bookManager/Book.java
+++ team01/src/bookManager/Book.java
@@ -1,5 +1,10 @@
 package bookManager;
 
+//	- Book 클래스 설계
+//	도서와 관련된 기본정보
+//	도서 대출 메소드 오버라이딩
+//	equals, hashCode 오버라이딩
+
 public class Book {
 
 }
--- team01/src/bookManager/Borrow.java
+++ team01/src/bookManager/Borrow.java
@@ -1,4 +1,6 @@
 package bookManager;
+//  Borrow 인터페이스
+//	추상 메소드 2개(대여 -> 예외 던지기, 반납)
 
 public interface Borrow {
 
