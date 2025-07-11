1983d7bfc82177b69f696a49104d07fd6b9e6c82
seojin12lee <aillsalovesophia@gmail.com>
2025-07-11 12:51:10 +0900

    250711 12:50 하던 BOOK 클래스 오버라이딩 작작업 1차 저장

--- team01/src/bookManager/Book.java
+++ team01/src/bookManager/Book.java
@@ -1,5 +1,90 @@
 package bookManager;
 
-public class Book {
+import java.util.ArrayList;
+import java.util.List;
 
+//	- Book 클래스 설계
+//	도서와 관련된 기본정보
+//	도서 대출 메소드 오버라이딩
+//	equals, hashCode 오버라이딩
+
+public class Book<T> extends Memeber {
+	// 필드
+	private int bookID; //책 id
+	private String bookTitle; // 책 제목
+	private String author; // 작가
+	private boolean isBorrow;// 빌린여부
+
+	// 생성자
+	public Book(int bookID, String bookTitle, String author, boolean isBorrow) {
+		super();
+		this.bookID = bookID;
+		this.bookTitle = bookTitle;
+		this.author = author;
+		this.isBorrow = isBorrow;
+	}
+	
+	//arraylist 선언
+	//책 목록 리스트 (20권 id는 1~20
+//	booklist.add(new Book(1,"title","auther",false));
+	
+	//메소드
+	//책 제목으로 리스트에서 책을 찾는 메소드
+	// -> 리스트를 반복자(iterator)를 사용하여 훑어본 다음에 
+	//책
+
+	/*--------오버라이딩-----------*/
+	// hashCode - 구분 대상은 bookID
+	@Override 
+	public int hashCode() {
+		return this.bookID;
+	}
+
+	// equals - 책 제목이 동일하다면
+	@Override
+	public boolean equals(Object obj) {
+		if(obj== this) {
+			return true;
+		}
+		if(obj instanceof Book) {
+			String other = ((Book)obj).bookTitle;
+			if(this.bookTitle.length() == other.length()) {
+				for(int i=0;i<bookTitle.length();i++) {
+					if(this.bookTitle.charAt(i)!= other.charAt(i)) {
+						return false;
+					}//책 제목 한글자씩 비교
+				}//책 제목만큼 반복 for
+			}//책.Title과 동일하다면
+			return true;
+		}
+		return false;
+	}
+	// 도서 대출 메소드
+
+	// getter, setter
+	public String getBookTitle() {
+		return bookTitle;
+	}
+
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public boolean isBorrow() {
+		return isBorrow;
+	}
+
+	public void setBorrow(boolean isBorrow) {
+		this.isBorrow = isBorrow;
+	}
+
+	
 }
---
+++ team01/src/bookManager/BookList.java
@@ -0,0 +1,12 @@
+package bookManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookList<T> {
+	List<Book> booklist = new ArrayList<>();
+	booklist.add(new Book(1,"코스모스","칼 세이건",false));
+	Book book2 = new Book(2,"데미안","헤르만 헤세",false);
+	book
+
+}
