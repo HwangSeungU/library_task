82bdc6ea2de78a9c129bc07075655424e475badb
jsj030513 <sungju03051323@gmail.com>
2025-07-15 09:05:44 +0900

    0715 0905

--- team01/src/memberManager/Member.java
+++ team01/src/memberManager/Member.java
@@ -12,44 +12,58 @@ public class Member extends User{//User로 바꿔서 해야함
 	
 	// List 필드 - 책 저장 리스트
 	ArrayList<Book> borrowedBooks = new ArrayList<>();
-	private final int MAX_BORROW = 20;
+	private final int MAX = 20;
 
-	// 생성자
-	public Member(String name, String phoneNumber, String id, String password, ArrayList<Book> borrowBooks) {
+	// 생성자 
+	public Member(String name, String phoneNumber, String id, String password, ArrayList<Book> borrowedBooks) {
 		super(name, phoneNumber, id, password);
+		this.borrowedBooks = borrowedBooks;
 	}
 
-	// 로그인 메소드 오버라이딩 -- 이 부분은 승우님이 다시 바꿔주시면 고칠게요!
 	
+	@Override
+	public String toString() {
+		return "대출한 책: " + borrowedBooks + ", 최대로 빌릴 수 있는 책 수: " + MAX + "]";
+	}
+
+
+	// 로그인 메소드 오버라이딩
 	//UserManager 메소드 전부 User로 이동시켜서 User에서 상속받아서 login 메소드 이용할 수 있게 적용
-//	@Override
+	@Override
 	public void login(String id, String password) {
 		super.login(id, password);
 		System.out.println("로그인 성공");
 	}
 	
 	// 도서 대출 메소드
-		//borrowBook 
+	//borrowBook 
+	//이미 책을 빌렸는지 최대로 빌린 상태인지 책이 사용상태인지
+	//대출이 되는 책인지 확인하기ㅎ
+	//최대 대출보다 작은지 확인 max로 상수 설정 후에 사용
+	//책이 사용가능 상태인지 확인 true false로 book에서 해주시면 감사할게요!!
+	//대출목록에 책 추가하기
+	//예외처리 
 	public void borrowBook(Book book) 
-			//  이미 책을 빌렸는지 최대로 빌린 상태인지 책이 사용상태인지
 			throws BookAlreadyException, MaxBorrowException, BookNotAvailableException  {
-		if(borrowedBooks.contains(book)) {//대출이 되는 책인지 확인
+		if(borrowedBooks.contains(book)) {
 			throw new BookAlreadyException();
 			}
-		if(borrowedBooks.size() >= MAX_BORROW) {//최대 대출보다 작은지 확인
+		if(borrowedBooks.size() >= MAX) {
 			throw new MaxBorrowException();
 			}
-		if(!book.isAvailable()) {//책이 사용가능 상태인지 확인
+		if(!book.isAvailable()) {
 			throw new BookNotAvailableException();
 		}
-		borrowedBooks.add(book); //대출목록에 책 추가
+		borrowedBooks.add(book); 
 		book.borrow();
 		System.out.println("대출 성공. 대출한 책 제목: " + book.getTitle());
 	}	
-	
+
 	// 도서 반남 메소드
 	//returnBook 책을 빌렸었는지 먼저 하기
-	public void returnBook(Book book) throws BookNotAvailableException {
+	//예외처리
+	public void returnBook(Book book) 
+			throws BookNotAvailableException {
 		if(!borrowedBooks.contains(book)) {
 			throw new BookNotAvailableException();
 			}
@@ -57,8 +71,10 @@ public class Member extends User{//User로 바꿔서 해야함
 		book.returnBook();
 		System.out.println("반납 성공. 반납한 책 제목: " + book.getTitle());
 	}
+	
 	// 대출 중인 책 메소드
-		//checkBooks 대출중인 책이 있는지 보기
+	//checkBooks 대출중인 책이 있는지 보기
+	//book 클래스에 Title getter로 해주시면 그대로 사용할게요
 	public void checkBooks() { 
 		if(borrowedBooks.isEmpty()) {
 			System.out.println("대출 중이 아닙니다");
