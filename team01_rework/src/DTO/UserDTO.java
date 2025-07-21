a3133e6fce23668c62251a986674df346f33bf6d
jsj030513 <sungju03051323@gmail.com>
2025-07-21 19:24:23 +0900

    250721 1924 userdto 생성

--- team01_rework/src/DTO/UserDTO.java
+++ team01_rework/src/DTO/UserDTO.java
@@ -1,5 +1,46 @@
 package DTO;
 
 public class UserDTO {
-
+	private int userNumber;
+	private String userId;
+	private String userPassword;
+	private String userName;
+	private String userPhoneNumber;
+	public int getUserNumber() {
+		return userNumber;
+	}
+	public void setUserNumber(int userNumber) {
+		this.userNumber = userNumber;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPhoneNumber() {
+		return userPhoneNumber;
+	}
+	public void setUserPhoneNumber(String userPhoneNumber) {
+		this.userPhoneNumber = userPhoneNumber;
+	}
+	@Override
+	public String toString() {
+		return "UserDTO [userNumber=" + userNumber + ", userId=" + userId + ", userPassword=" + userPassword
+				+ ", userName=" + userName + ", userPhoneNumber=" + userPhoneNumber + "]";
+	}
+	
+	
 }
