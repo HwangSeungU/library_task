7a517ff4606a5c44880b60ab1d827fc35ced7ab0
songeunkyeong99 <ruddlgkxn@gmail.com>
2025-07-22 21:25:06 +0900

    250722 21:24 test 수정(consoleUI)

--- team01_rework/src/test/Test.java
+++ team01_rework/src/test/Test.java
@@ -1,7 +1,19 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import DAO.UserDAO;
 import DTO.UserDTO;
+import bookManager.Book;
+import bookManager.BookList;
+import exceptionManager.BookAlreadyException;
+import exceptionManager.BookNotAvailableException;
+import exceptionManager.MaxBorrowException;
+import memberManager.Member;
+import serviceManager.LibraryService;
+import useManager.ConsoleUI;
+import userManager.User;
 
 public class Test {
 	public static void main(String[] args) {
@@ -32,7 +44,138 @@ public class Test {
 		
 		
 		
+		
+		
+		
+//		===================sek===============
+		
+		Scanner sc=new Scanner(System.in);
+		Test ts=new Test();
+		ts.login(sc);
+
+	public void login(Scanner sc) {
+
+		while (true) {
+			String setUserId = "";
+			String setUerPassword = "";
+			String setUserPhoneNumber = "";
+			String setUserName = "";
+			Test.menu("로그인", "회원가입", "프로그램 종료");
+			button = sc.nextInt();
+			sc.nextLine();
+			switch (button) {
+			case 1:
+				// 로그인 메소드
+				System.out.println("로그인");
+				System.out.print("ID 입력 : \n");
+				setUserId = sc.nextLine();
+				System.out.print("PASSWORD 입력 : \n");
+				setUerPassword = sc.nextLine();
+				User loginU = User.login(setUserId, setUerPassword); // ----------v
+				if (loginU != null) {
+					consol(sc, loginU);
+				} else {
+					System.out.println("프로그램을 강제로 종료합니다.");
+					return;
+				}
+				break;
+			case 2:
+				// 회원가입 메소드
+				System.out.println("회원가입");
+				System.out.print("이름 : \n");
+				setUserName = sc.nextLine();
+				System.out.print("전화번호 : \n");
+				setUserPhoneNumber = sc.nextLine();
+				System.out.print("아이디 : ");
+				setUserId = sc.nextLine();
+				System.out.print("비밀번호 : ");
+				setUerPassword = sc.nextLine();
+				User addUser = new Member(setUserName, setUserPhoneNumber, setUserId, setUerPassword,
+						new ArrayList<>());// -----------------v
+				User.signUpUser(addUser);// ----------------v
+				break;
+			case 3:
+				// 종료 메소드
+				System.out.println("프로그램을 종료합니다");
+				break;
+			default:
+				System.out.println("제시된 번호 외의 다른 번호를 입력하지 말아주세요.");
+				break;
+			} // switch
+			if (button == 3) {
+				break;
+			}
+		}
 	}
-	
+
+	public void consol(Scanner sc, User addUser) { //----------v
+
+		LibraryService ls = new LibraryService();//---------v
+
+		while (true) {
+			Test.menu("목록 확인", "책 대출하기", "반납 하기", "내 대여 목록 확인", "뒤로가기");
+			Member member = (Member) addUser;//----------v
+			int bookId = 0;
+			BookList bl = new BookList();//---------v
+			Book searchIdBook = null;
+			button = sc.nextInt();
+			sc.nextLine();
+			switch (button) {
+			case 1:
+				// 목록확인 메소드
+				BookList.printAll();//-----------v
+				break;
+			case 2:
+				System.out.print("대출할 책 ID를 입력해주세요 : ");
+				bookId = sc.nextInt();
+				sc.nextLine();
+				searchIdBook = BookList.searchIdBook(bookId);//---------v
+//        	 System.out.println(searchIdBook);
+//        	 System.out.println(addUser.getName());
+				try {
+					ls.borrow(member, searchIdBook.getTitle());
+				} catch (BookAlreadyException | MaxBorrowException | BookNotAvailableException e) {
+					e.printStackTrace();//---------v
+				}
+				break;
+			case 3:
+				// 반납하는 메소드
+				System.out.println("반납하실 도서의 id를 입력해주세요");
+				bookId = sc.nextInt();
+				sc.nextLine();
+				searchIdBook = BookList.searchIdBook(bookId);//-----------v
+				try {
+					ls.returnBook(member, searchIdBook.setBookTitle());
+				} catch (BookNotAvailableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case 4:
+				member.checkBook();
+				break;
+			case 5:
+				// 완전 종료
+				System.out.println("뒤로가기");
+				return;
+			default:
+				System.out.println("제시된 번호만 입력해주세요. 제발");
+				break;
+			} // switch
+		} // while
+	}
+
+	static public void menu(String... menuNames) {
+		int count = 1;
+		System.out.println("=================================");
+		for (String menu : menuNames) {
+			System.out.println("=\t\t\t\t=");
+			System.out.println("=\t" + count + ". " + menu);
+			System.out.println("=\t\t\t\t");
+			count++;
+		}
+		System.out.println("=\t\t\t\t");
+
+	}
 
 }
