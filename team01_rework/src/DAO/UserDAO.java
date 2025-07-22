6767ed349310d0c10bba1b6dde3505dc2a2fe810
HwangSeungU <pett1475321@gmail.com>
2025-07-23 00:01:50 +0900

    250723 1201 메소드 구성 완료

--- team01_rework/src/DAO/UserDAO.java
+++ team01_rework/src/DAO/UserDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import DTO.UserDTO;
 
@@ -11,33 +13,33 @@ public class UserDAO {
 	public Connection connection;
 	public PreparedStatement preparedStatement;
 	public ResultSet resultSet;
-	
-	//회원 추가
+
+	// 회원 추가
 	public void userSign(UserDTO userDTO) {
 		String query = "INSERT INTO TBL_USER(USER_NUMBER, USER_ID, USER_PASSWORD, USER_NAME, USER_PHONE_NUMBER) "
 				+ "VALUES(SEQ_USER.NEXTVAL, ?, ?, ?, ?)";
 		connection = DBConnecter.getConnection();
-		
+
 		String exceptSlash = userDTO.getUserPhoneNumber().replaceAll("-", "");
-		
+
 		try {
 			preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setString(1, userDTO.getUserId());
 			preparedStatement.setString(2, userDTO.getUserPassword());
 			preparedStatement.setString(3, userDTO.getUserName());
 			preparedStatement.setString(4, exceptSlash);
-			
+
 			preparedStatement.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			System.out.println("userSign() 메소드(회원가입) 쿼리문 오류");
 			e.printStackTrace();
 		} finally {
 			try {
-				if(preparedStatement != null) {
+				if (preparedStatement != null) {
 					preparedStatement.close();
 				}
-				if(connection != null) {
+				if (connection != null) {
 					connection.close();
 				}
 			} catch (SQLException e) {
@@ -47,9 +49,8 @@ public class UserDAO {
 			}
 		}
 	}
-	
-	
-	//회원 삭제
+
+	// 회원 삭제
 	public boolean userDelete(int userNumber) {
 		String query = "DELETE TBL_USER WHERE USER_NUMBER = ?";
 		int result = 0;
@@ -57,11 +58,174 @@ public class UserDAO {
 		try {
 			preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setInt(1, userNumber);
-			
+
 			result = preparedStatement.executeUpdate();
 		} catch (SQLException e) {
 			System.out.println("userDelelte() 쿼리문 오류");
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (preparedStatement != null) {
+					preparedStatement.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("닫힐 때 오류");
+				e.printStackTrace();
+			}
+		}
+		return result > 0;
+	}
+
+	// 로그인
+	public int loginUser(String userId, String userPassword) {
+		String query = "SELECT USER_NUMBER FROM TBL_USER WHERE USER_ID = ? AND USER_PASSWORD= ? ";
+		int userNumber = -1;
+
+		connection = DBConnecter.getConnection();
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, userId);
+			preparedStatement.setString(2, userPassword);
+
+			resultSet = preparedStatement.executeQuery();
+			if (resultSet.next()) {
+				userNumber = resultSet.getInt(1);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("loginUser() 쿼리문 오류");
+			e.printStackTrace();
+		} finally {
+			try {
+				if (resultSet != null) {
+					resultSet.close();
+				}
+				if (preparedStatement != null) {
+					preparedStatement.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return userNumber;
+
+	};
+
+	// 아이디 찾기
+	public List<String> findId(String userName, String userPhoneNumber) {
+		String query = "SELECT USER_ID FROM " + "TBL_USER WHERE USER_NAME = ? AND USER_PHONE_NUMBER = ?";
+		List<String> userIds = new ArrayList<>();
+
+		String exceptSlash = userPhoneNumber.replaceAll("-", "");
+
+		connection = DBConnecter.getConnection();
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, userName);
+			preparedStatement.setString(2, exceptSlash);
+
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+				String userId = resultSet.getString("USER_ID");
+				userIds.add(userId);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("findId() 쿼리문 오류");
+			e.printStackTrace();
+		} finally {
+			try {
+				if (resultSet != null) {
+					resultSet.close();
+				}
+				if (preparedStatement != null) {
+					preparedStatement.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return userIds;
+	}
+
+	// 비밀번호 찾기
+	public List<String> findPassword(String userName, String userId, String userPhoneNumber) {
+		String query = "SELECT USER_PASSWORD FROM "
+				+ "TBL_USER WHERE USER_NAME = ? AND USER_ID = ? AND USER_PHONE_NUMBER = ?";
+		List<String> userPws = new ArrayList<>();
+
+		String exceptSlash = userPhoneNumber.replaceAll("-", "");
+
+		connection = DBConnecter.getConnection();
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, userName);
+			preparedStatement.setString(2, userId);
+			preparedStatement.setString(3, exceptSlash);
+
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+				String userPassword = resultSet.getString("USER_PASSWORD");
+				userPws.add(userPassword);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("findPassword() 쿼리문 오류");
+			e.printStackTrace();
+		} finally {
+			try {
+				if (resultSet != null) {
+					resultSet.close();
+				}
+				if (preparedStatement != null) {
+					preparedStatement.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return userPws;
+	}
+
+	// 회원정보 수정
+	public boolean updateUser(String userName, String userPassword, int userNumber) {
+		String query = "UPDATE tbl_user SET USER_NAME = ? , USER_PASSWORD = ? WHERE user_number = ? ";
+		connection = DBConnecter.getConnection();
+		int result = 0;
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, userName);
+			preparedStatement.setString(2, userPassword);
+			preparedStatement.setInt(3, userNumber);
+
+	        result = preparedStatement.executeUpdate(); // 업데이트된 행 수
+	       
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("updateUser() 쿼리문 오류");
 			e.printStackTrace();
 		} finally {
 			try {
@@ -73,21 +237,52 @@ public class UserDAO {
 				}
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
-				System.out.println("닫힐 때 오류");
 				e.printStackTrace();
 			}
 		}
-		return result>0;
+		return result > 0;
 	}
-	
-	//로그인(작업중)
-	public int loginUser(String userId, String userPassword){
-		String query = "SELECT USER_NUMBER FROM TBL_USER WHERE USER_ID = ? AND USER_PASSWORD= ? ";
+
+	// 회원 정보 찾기
+	public int findUser(String userId, String userPassword, String userPhoneNumber) {
+		String query = "SELECT USER_NUMBER FROM TBL_USER WHERE (USER_ID = ? AND USER_PASSWORD = ? ) AND USER_PHONE_NUMBER = ?";
+		String exceptSlash = userPhoneNumber.replaceAll("-", "");
+		
+		int result;
+		result= -1;
+		
 		connection = DBConnecter.getConnection();
-		return 0;
-	};
-	
-	
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, userId);
+			preparedStatement.setString(2, userPassword);
+			preparedStatement.setString(3, exceptSlash);
+			resultSet = preparedStatement.executeQuery();
+			
+			if(resultSet.next()) {
+				result = resultSet.getInt(1);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("쿼리 오류");
+			e.printStackTrace();
+		} finally {
+			try {
+				if(resultSet != null) {
+					resultSet.close();
+				}
+				if(preparedStatement != null) {
+					preparedStatement.close();
+				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
 }
-
-
